6627ce8ee9b8f74ec2c0b040532a840f444d4e2e
Jonathan Alba Videira <jonathan.videira@gmail.com>
2021-03-24 07:51:07 -0300

    Implemented server stream and client

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/ajvideira/fullcycle-grpc/pb"
@@ -19,10 +20,10 @@ func main() {
 
 	defer connection.Close()
 
-	client := pb.NewUserServiceClient(connection);
+	client := pb.NewUserServiceClient(connection)
 
 	addUser(client)
-
+	addUserVerbose(client)
 }
 
 func addUser(client pb.UserServiceClient) {
@@ -31,10 +32,34 @@ func addUser(client pb.UserServiceClient) {
 		Email: "jonathan.videira@gmail.com",
 	}
 
-	res, err := client.AddUser(context.Background(), req);
+	res, err := client.AddUser(context.Background(), req)
 	if err != nil {
 		log.Fatalf("Could not call AddUser: %v", err)
 	}
 
 	fmt.Println(res)
+}
+
+func addUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Name: "Jonathan",
+		Email: "jonathan.videira@gmail.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not call AddUser: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv();
+		if err == io.EOF {
+			fmt.Println("End of stream.")
+			break
+		} 
+		if err != nil {
+			log.Fatalf("Error receiving the stream: %v", err)
+		}
+		fmt.Println(stream.GetStatus())
+	}
 }
--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/ajvideira/fullcycle-grpc/pb"
 )
@@ -24,4 +25,31 @@ func (userService *UserService) AddUser(ctx context.Context, req *pb.User) (*pb.
 		Name: req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (userService *UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Preparing Insert",
+		User: nil,
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User: nil,
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Insert Complete",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	return nil
 }
